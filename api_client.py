0011e92834a76074d4dd84df5db5d72b3340f237
CrisitanLalangui <cristianalg741@gmail.com>
2026-03-01 17:16:17 +0100

    subiendo cambios

--- api_client.py
+++ api_client.py
@@ -13,3 +13,84 @@ def get_weather(lat, lon):
     data = response.json()
 
     return data["current_weather"]
+
+def get_weatherDetail(lat,lon):
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": lat,
+        "longitude": lon,
+        "current_weather": True
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    return data
+
+
+def get_ciudad(ciudad):
+
+    ciudadCorrecta = False
+    url = "https://geocoding-api.open-meteo.com/v1/search?name="
+
+    response = requests.get(url + ciudad)
+
+    data = response.json()
+
+    if "results" not in data:
+        return None
+
+        # Buscar coincidencia exacta
+    for resultado in data["results"]:
+        if resultado["name"].lower() == ciudad.lower():
+            ciudadCorrecta =  True
+
+    print(ciudad)
+    return ciudadCorrecta
+
+
+def get_pais(pais):
+
+    paisCorrecto = False
+    url = "https://geocoding-api.open-meteo.com/v1/search?name="
+
+    response = requests.get(url + pais)
+
+    data = response.json()
+
+    if "results" not in data:
+        return None
+
+        # Buscar coincidencia exacta
+    for resultado in data["results"]:
+        if resultado["name"].lower() == pais.lower():
+            paisCorrecto =  True
+
+    print(pais)
+
+    return paisCorrecto
+
+def get_coordenadas(ciudad,pais):
+
+
+    lat = None
+    lon = None
+    url = "https://geocoding-api.open-meteo.com/v1/search?name="
+
+    response = requests.get(url + ciudad)
+
+    data = response.json()
+
+    if "results" not in data:
+        return None
+
+        # Buscar coincidencia exacta
+    for resultado in data["results"]:
+        if resultado["name"].lower() == ciudad.lower() and resultado["country"].lower()==pais.lower():
+
+           lat = resultado["latitude"]
+           lon = resultado["longitude"]
+
+    print(pais)
+
+    return lat, lon
--- main.py
+++ main.py
@@ -1,9 +1,95 @@
-from api_client import get_weather
+from api_client import get_weather, get_ciudad, get_pais, get_coordenadas, get_weatherDetail
 
-lat = 40.4168
-lon = -3.7038
+from translate import Translator
+traductor = Translator(to_lang="en")
 
-weather = get_weather(lat, lon)
 
-print("Temperatura:", weather["temperature"])
-print("Viento:", weather["windspeed"])
+def dia_o_noche(parametro):
+    if parametro == 1:
+        print("Actualmente es de día")
+    else:
+        print("Actualmente es de noche")
+
+def direccion_viento(grados):
+
+    direccion = None
+    if (grados >= 337.5 or grados < 22.5):
+        direccion =  "Norte"
+    elif grados < 67.5:
+        direccion = "Noreste"
+    elif grados < 112.5:
+        direccion =  "Este"
+    elif grados < 157.5:
+        direccion = "Sureste"
+    elif grados < 202.5:
+        direccion =  "Sur"
+    elif grados < 247.5:
+        direccion =   "Suroeste"
+    elif grados < 292.5:
+        direccion = "Oeste"
+    else:
+        direccion = "Noroeste"
+
+    return direccion
+
+def estadoTiempo(parametro):
+
+    estado = None
+    if parametro == 0:
+        estado = "Despejado"
+    elif parametro == 1:
+        estado = "Parcialmente nublado"
+    elif parametro == 2:
+        estado = "Nublado"
+    elif parametro == 3:
+        estado = "Muy nublado"
+
+    return  estado
+
+
+pais = input("Introduce un país: ")
+pais = pais.capitalize()
+
+pais = traductor.translate(pais)
+pais = pais.strip()
+print(pais)
+
+
+while not get_pais(pais):
+    print("El país introducido  no existe ")
+    pais = input("Por favor introduzca un país correcto: ")
+    pais = pais.strip()
+
+ciudad = input("Introduce una ciudad: ")
+ciudad = ciudad.capitalize()
+ciudad = ciudad.strip()
+print(ciudad)
+
+while not get_ciudad(ciudad):
+    print("La ciudad  introducida no existe ")
+    ciudad = input("Por favor introduzca una ciudad correcta: ")
+    ciudad = ciudad.strip()
+
+
+
+latitud,longitud = get_coordenadas(ciudad,pais)
+print(latitud,longitud)
+
+print(f"***************TIEMPO EN {ciudad.upper()}******************")
+weather = get_weather(latitud, longitud)
+weather_detail = get_weatherDetail(latitud,longitud)
+
+print("Temperatura :", weather["temperature"] , "Cº")
+print("Viento:", weather["windspeed"], "km/h")
+print("Altitud sobre el nivel del mar: ",weather_detail["elevation"], "metros")
+dia_o_noche(weather["is_day"])
+
+print("Dirección del viento: ",direccion_viento(weather["winddirection"]))
+
+print("Estado del clima: ", estadoTiempo(weather["weathercode"]))
+
+
+print("\nRespuesta generada en: ", weather_detail["generationtime_ms"], "milisegundos ")
+
+
+
